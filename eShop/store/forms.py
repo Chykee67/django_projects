f43b3cca6d9496af5fe9e398240c2f8fef82bd3d
chikeluba <jpokochike@gmail.com>
2024-08-26 00:23:56 +0100

    further editing user_index page, store forms(perhaps better to use django admin)

---
+++ eShop/store/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class CategoryForms():
+    """ Form for creating categories """
+
+    category_description = 
--- eShop/store/views.py
+++ eShop/store/views.py
@@ -1,14 +1,40 @@
+import emoji
+
 from django.shortcuts import render
 
 from login.models import User
+from .models import Category, Subcategory, Item
+
 
 def IndexView(request, user_name):
 
-    user = User.objects.get(user_name=user_name)
+    categories = Category.objects.all()
 
-    if user.user_name == 'chykee67':
-        return render(request, 'store/admin_index.html')
+    try:
+        user = User.objects.get(user_name=user_name)
+    except(KeyError, User.DoesNotExist):
+        return render(request, 'store/user_index.html', IndexViewContext(user_name))
+
     else:
-        return render(request, 'store/user_index.html')
-    
+        if user.user_name == 'chykee67':
+            return render(request, 'store/admin_index.html', IndexViewContext(user_name))
+
+        else:
+            return render(request, 'store/user_index.html', IndexViewContext(user_name))
+
+def IndexViewContext(user_name):
+    try:
+        user = User.objects.get(user_name=user_name)
+    except (KeyError, User.DoesNotExist):
+        return {
+            'cheers': emoji.emojize(":party_popper:"),
+            'categories': Category.objects.all(),
+        }
+    else:
+        return {
+            'user': user,
+            'cheers': emoji.emojize(":party_popper:"),
+            'categories': Category.objects.all(),
+        }
+
 # Create your views here.
