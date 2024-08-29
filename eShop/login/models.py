d89bfadf8d0c2cfa0a67ca7393626cd556f268f4
chikeluba <jpokochike@gmail.com>
2024-08-29 17:28:53 +0100

    still working on login models for user that is compatible with django auth

--- eShop/login/models.py
+++ eShop/login/models.py
@@ -4,12 +4,59 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 
+class CustomUserManager(BaseUserManager):
 
-class User(AbstractUser):
-    username = models.CharField(
-        max_length = 40,
-        unique=True,
-    )
+    """ Manager for User model below """
+
+    def create_user(
+        self, email, date_of_birth, country_of_residence, state_of_residence, city_of_residence, street_address, first_name, last_name, password=None,
+        )
+
+        """
+            creates and saves user to database.
+        """
+
+        if not email:
+            raise ValueError("User must have an email address!")
+
+
+        user = self.model(
+            email=self.normalize_email(email), date_of_birth=date_of_birth,
+            country_of_residence=country_of_residence, state_of_residence=state_of_residence,
+            city_of_residence=city_of_residence, street_address=street_address,
+            first_name=first_name, last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+
+    def create_superuser(
+        self, email, date_of_birth, country_of_residence, state_of_residence, city_of_residence, street_address, first_name, last_name, password,
+        )
+
+        """
+            creates and saves a superuser to database.
+        """
+
+        user = self.create_user(
+            email=email, date_of_birth=date_of_birth,
+            country_of_residence=country_of_residence, state_of_residence=state_of_residence,
+            city_of_residence=city_of_residence, street_address=street_address,
+            first_name=first_name, last_name=last_name,
+            password=password,
+            )
+
+        user.is_admin = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
 
     first_name = models.CharField(
         max_length = 40,
@@ -72,34 +119,6 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
-class CustomUserManager(BaseUserManager):
 
-    """ Manager for User model above """
-
-    def create_user(
-        self,
-        username,
-        email,
-        date_of_birth,
-        country_of_residence,
-        state_of_residence,
-        city_of_residence,
-        street_address,
-        first_name,
-        last_name,
-        password=None,
-    )
-
-        return User(
-            username=username,
-            email=email,
-            date_of_birth=date_of_birth,
-            country_of_residence=country_of_residence,
-            state_of_residence=state_of_residence,
-            city_of_residence=city_of_residence,
-            street_address=street_address,
-            first_name=first_name,
-            last_name=last_name,
-        )
 
         #stopped at page 583
